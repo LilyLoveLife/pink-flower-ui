acde276be9c7ad9bc3bad9f4b6bcaaff80ebc3e6
Lily <lily.song@hrtps.com>
2023-05-26 10:05:17 +0800

    feat:  modules.css

--- packages/components/src/Button/index.tsx
+++ packages/components/src/Button/index.tsx
@@ -4,7 +4,7 @@
  * @Author: 
  * @Date: 2023-04-02 22:54:11
  * @LastEditors: Lily lily.song@hrtps.com
- * @LastEditTime: 2023-05-23 20:19:10
+ * @LastEditTime: 2023-05-25 19:43:03
  */
 // import React, {useState} from 'react'
 import React from 'react'
@@ -12,8 +12,8 @@ import {Button} from 'antd'
 import { ButtonProps, ButtonType } from 'antd/lib/button'
 // import React from '@vitejs/plugin-react'
 import cs from 'classnames'
-// import styles from './index.module.scss'
-import  './index.css'
+import styles from './index.module.scss'
+// import  './index.css'
 
 interface IProps extends Omit<ButtonProps, 'type'> {
     type?: 'login' | ButtonType
@@ -43,14 +43,14 @@ const PinkButton = (props: IProps, ref: any) => {
     //     } = props
     //     return <Button {...props} ref={ref}></Button>
     // }
-    // let defaultClassName = cs({
-    //     [styles.customBtn]: true,
-    //     [styles.pink]: true,
-    //     [styles.login]: type === 'login',
-    // })
-      // const className = customClassName ? `${defaultClassName} ${customClassName}` : defaultClassName
-    // return <Button  type={['login'].includes(type) ? 'primary' : type} className={className} {...otherProps} ></Button>
-    return <Button  type={['login'].includes(type) ? 'primary' : type} className={'pink'} >submit</Button>
+    let defaultClassName = cs({
+        [styles.customBtn]: true,
+        [styles.pink]: true,
+        [styles.login]: type === 'login',
+    })
+      const className = customClassName ? `${defaultClassName} ${customClassName}` : defaultClassName
+    return <Button  type={['login'].includes(type) ? 'primary' : type} className={className} {...otherProps} ></Button>
+    // return <Button  type={['login'].includes(type) ? 'primary' : type} className={'pink'} >submit</Button>
 
 }
 Object.assign(PinkButton, Button)
