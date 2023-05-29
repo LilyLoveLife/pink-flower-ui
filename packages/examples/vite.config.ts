beb0ee0e1fa5f00c9fabbfc13e5dc8074b37458a
Lily <lily.song@hrtps.com>
2023-05-29 19:34:09 +0800

    feat: test only btn

--- packages/examples/vite.config.ts
+++ packages/examples/vite.config.ts
@@ -3,8 +3,8 @@
  * @version: 
  * @Author: 
  * @Date: 2023-05-07 17:20:52
- * @LastEditors: Please set LastEditors
- * @LastEditTime: 2023-05-29 00:58:10
+ * @LastEditors: Lily lily.song@hrtps.com
+ * @LastEditTime: 2023-05-29 18:20:31
  */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
