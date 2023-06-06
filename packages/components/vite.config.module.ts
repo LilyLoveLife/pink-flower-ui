24f2f7dae37b7fc8f5b81d789ad87433da123cae
Lily <lily.song@hrtps.com>
2023-06-06 14:44:17 +0800

    feat: add desc

--- packages/components/vite.config.module.ts
+++ packages/components/vite.config.module.ts
@@ -2,7 +2,7 @@
  * @Author: Lily lily.song@hrtps.com
  * @Date: 2023-05-08 11:29:24
  * @LastEditors: Lily lily.song@hrtps.com
- * @LastEditTime: 2023-06-05 21:10:31
+ * @LastEditTime: 2023-06-06 14:43:34
  * @FilePath: /theseus-cooperation/Users/hrtps/Documents/Projects/pink-ui/packages/components/vite.config.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -35,7 +35,7 @@ export const config = (moduleName: string) => {
     return {
         plugins: [
             typescript({
-                rootDir: `./src/${moduleName}`,
+                rootDir: `./src/${moduleName}`, // 项目根目录，这里分模块打包，则根目录变成模块入口
                 outDir:`./dist/es/${moduleName}`,
                 declaration: true,
                 declarationDir: `./dist/es/${moduleName}`,
