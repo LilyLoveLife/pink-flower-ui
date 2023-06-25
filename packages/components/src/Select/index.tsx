85de340f5da4a9d5719a6f2874a76dbeb54cfb89
LilyLoveLife <LilySong201608@163.com>
2023-06-25 15:19:29 +0800

    feat: eslint

--- packages/components/src/Select/index.tsx
+++ packages/components/src/Select/index.tsx
@@ -4,7 +4,7 @@
  * @Author: 
  * @Date: 2023-05-28 15:21:00
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2023-06-25 15:14:10
+ * @LastEditTime: 2023-06-25 15:19:14
  */
 import { Select as AntDSelect } from 'antd'
 import { SelectProps } from 'antd/lib/select'
@@ -12,13 +12,13 @@ import styles from './index.module.scss'
 import cs from 'classnames'
 import lodash from 'lodash'
 const PinkSelect: any = (props: SelectProps) => {
-  props.test = 2
+  props.test = 3
   const { className: customClassName, popupClassName: customPopupClassName,  ...otherProps }: any = props // children,
   let defaultClassName = cs({
       [styles.pinkSelect]: true,
   })
   const className = customClassName ? `${defaultClassName} ${customClassName}` : defaultClassName
-  return <AntDSelect id="1" className={className} placeholder='请选择' {...otherProps}></AntDSelect>
+  return <AntDSelect id='3 className={className} placeholder='请选择' {...otherProps}></AntDSelect>
 }
 PinkSelect.Option = AntDSelect.Option
 PinkSelect.OptGroup = AntDSelect.OptGroup
