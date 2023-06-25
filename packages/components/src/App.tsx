7da883fb91e2d29ade2e7a6ec0d0d4a21d1ded87
LilyLoveLife <LilySong201608@163.com>
2023-06-25 23:40:13 +0800

    test

--- packages/components/src/App.tsx
+++ packages/components/src/App.tsx
@@ -3,7 +3,8 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "../../../../../../../../vite.svg";
 import "./App.css";
 import Button from "./Button";
-let a = 'test' 
+
+const [count1, setCountq1] = useState(0); 
 const App = () => {
   const [count, setCount] = useState(0);
 
