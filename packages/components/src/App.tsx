a838e6b50064a39181b324ca4b171bfb61ad884f
LilyLoveLife <LilySong201608@163.com>
2023-06-26 02:24:08 +0800

    test

--- packages/components/src/App.tsx
+++ packages/components/src/App.tsx
@@ -4,7 +4,7 @@ import viteLogo from "../../../../../../../../vite.svg";
 import "./App.css";
 import Button from "./Button";
 
-const [count7, setCount3] = useState(0);
+const [count7, setCount2] = useState(0);
 const App = () => {
   const [count, setCount] = useState(0);
 
