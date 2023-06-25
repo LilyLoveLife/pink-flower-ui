90ed2594b0f69b85dd38a17b356e33335f58a3e8
LilyLoveLife <LilySong201608@163.com>
2023-06-26 01:57:28 +0800

    feat: exmpale

--- packages/examples/src/App.tsx
+++ packages/examples/src/App.tsx
@@ -4,14 +4,12 @@
  * @Author: 
  * @Date: 2023-05-07 17:20:52
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2023-06-03 23:15:04
+ * @LastEditTime: 2023-06-25 19:56:55
  */
 import React , { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-// import Button from '../../components/src/Button'
-//import Button from '@pink-ui/components/dist/Button'
 import {Button, Select} from '@pink-ui/components'
 function App() {
   const [count, setCount] = useState(0)
@@ -30,25 +28,16 @@ function App() {
           <Select.Option value={1}>选项1</Select.Option>
           <Select.Option value={2}>选项2</Select.Option>
         </Select> */}
-        <a href="https://vitejs.dev" target="_blank">
+        {/* <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </a> */}
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
